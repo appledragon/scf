d3301e992573ea98afcf3afe8ba7ccec4d81f316
Xiaolong Cao <dragon.cao@visla.us>
2022-07-20 15:45:43 +0800

    search current file
    This reverts commit c16f4e7712d29cd3e3fb8465db58e23fbb759314.

--- src/extension.ts
+++ src/extension.ts
@@ -8,12 +8,12 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "singledcf" is now active!');
+	console.log('Congratulations, your extension "scf" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.dcf',async () => {
+	let disposable = vscode.commands.registerCommand('extension.scf',async () => {
 		await searchInCurrentFile();
 	});
 	context.subscriptions.push(disposable);
